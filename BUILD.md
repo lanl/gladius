b21c54185f52231ead547b03489f08c99e03b65c
Samuel K. Gutierrez <samuel@lanl.gov>
2014-10-09 10:57:32 -0600

    More LLDB configury.

---
+++ BUILD.md
@@ -0,0 +1,16 @@
+# Building Gladius
+
+## Prerequisites
+- C++11-capable C++ compiler
+- swig (http://www.swig.org/)
+- LLDB (http://lldb.llvm.org/)
+- MRNet (http://www.paradyn.org/mrnet/)
+
+## Building LLDB (see: http://lldb.llvm.org/build.html)
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+svn co http://llvm.org/svn/llvm-project/lldb/trunk lldb
+cd .. && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/local/lldb ../
+make && make install
--- cmake/FindLLDB.cmake
+++ cmake/FindLLDB.cmake
@@ -6,6 +6,38 @@
 # top-level directory of this distribution.
 #
 
-function(findLLDB outCXXFlags outLDFlags)
-    message(STATUS "Looking for LLDB")
+set(
+    GLADIUS_LLDB_HOME
+    "" CACHE FILEPATH
+    "The base directory of the LLDB installation that will be used to build
+    gladius. (Required)"
+)
+
+function(findLLDB outIncludeDirs outLinkDirs outLinkLibs)
+    if (GLADIUS_LLDB_HOME STREQUAL "")
+        message(FATAL_ERROR "GLADIUS_LLDB_HOME not defined. Cannot continue.")
+    endif()
+    set(prefix ${GLADIUS_LLDB_HOME})
+    message(STATUS "Looking for LLDB in ${prefix}")
+    message(STATUS "Making sure ${prefix} is a valid path...")
+    if(NOT EXISTS ${prefix})
+        message(FATAL_ERROR "${prefix} does not exist. Cannot continue")
+    endif()
+    message(STATUS "Making sure ${prefix} has some files we need...")
+    # FIXME add proper, os-specific extensions for libraries
+    set(fileWeNeed
+        "include/lldb/API/LLDB.h"
+        "lib/liblldb.dylib"
+    )
+    foreach (file ${fileWeNeed})
+        message(STATUS "Looking for ${file} in ${prefix}")
+        if(NOT EXISTS "${prefix}/${file}")
+            message(FATAL_ERROR "Cannot find ${file} in ${prefix}."
+                                "Cannot continue.")
+        endif()
+    endforeach()
+    # If we are here, then all is well. Return the outs.
+    set(${outIncludeDirs} "${prefix}/include" PARENT_SCOPE)
+    set(${outLinkDirs} "${prefix}/lib" PARENT_SCOPE)
+    set(${outLinkLibs} "lldb" PARENT_SCOPE)
 endfunction()
