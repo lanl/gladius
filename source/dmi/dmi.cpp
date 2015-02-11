a9590cad16c1d8b074f5cabaec687936a7ad4c30
Samuel K. Gutierrez <samuel@lanl.gov>
2015-02-11 15:23:35 -0700

    Start of DMI work.
    Debugger Machine Interface work.

---
+++ source/dmi/dmi.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the Gladius project. See the LICENSE.txt file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * Implements a thin shim over libmigdb.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "dmi/dmi.h"
---
+++ source/dmi/dmi.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the Gladius project. See the LICENSE.txt file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * The Debugger Machine Interface (DMI).
+ */
+
+#ifndef GLADIUS_DMI_DMI_H_INCLUDED
+#define GLADIUS_DMI_DMI_H_INCLUDED
+
+namespace gladius {
+namespace dmi {
+
+
+} // end dmi namespace
+} // end gladius namespace
+
+#endif
