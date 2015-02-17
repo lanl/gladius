89c00d1e9645ce9a6e31d9303db6c5682a5ed996
Samuel K. Gutierrez <samuel@lanl.gov>
2015-02-17 14:09:46 -0700

    Start of the Domain-Specific Plugin Architecture.

---
+++ source/dspa/dspa.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2015      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the Gladius project. See the LICENSE.txt file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * Implements the DSPA.
+ */
---
+++ source/dspa/dspa.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the Gladius project. See the LICENSE.txt file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * The Domain-Specific Plugin Architecture. 
+ */
+
+#ifndef GLADIUS_DSPA_DSPA_H_INCLUDED
+#define GLADIUS_DSPA_DSPA_H_INCLUDED
+
+namespace gladius {
+namespace dspa {
+
+/**
+ * Domain-Specific Plugin Architecture (DSPA). 
+ */
+class DSPA {
+public:
+    //
+    DSPA(void);
+    //
+    ~DSPA(void);
+};
+
+
+} // end dspa namespace
+} // end gladius namespace
+
+#endif
