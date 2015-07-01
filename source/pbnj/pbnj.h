a20bd48db749757157700783213e3b820b3b8631
Samuel K. Gutierrez <samuel@lanl.gov>
2015-07-01 12:15:17 -0600

    Start of PB&J.
    Mmm.

---
+++ source/pbnj/pbnj.h
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2015      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the Gladius project. See the LICENSE.txt file at the
+ * top-level directory of this distribution.
+ */
+
+#ifndef PBNJ_H_INCLUDED
+#define PBNJ_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int
+PBNJInit(void);
+
+/* in case you need this */
+#define LIBPBNJ 1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
